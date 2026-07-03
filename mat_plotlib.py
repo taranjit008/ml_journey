f0bfebab30736cd613be142f4b8f35a4b084eb37
Taranjit singh <ji8732927@gmail.com>
2026-07-03 22:31:32 +0530

    made line graph

--- mat_plotlib.py
+++ mat_plotlib.py
@@ -2,6 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+
+#Resize your Graph
+plt.figure(figsize=(5,3),dpi=300)#here figsize=(x,y) . we used to adjust the graph size.
+#dpi = dots per inch(image resolution) , higher the dpi , higher the resolution
+
 x=[0,1,2,3,4]
 y=[0,2,4,4,4]
 plt.plot(x,y,label="2x",color='green',marker='.',linestyle='--',linewidth=2,markersize=10,markeredgecolor='blue')
@@ -14,14 +19,30 @@ plt.plot(x,y,label="2x",color='green',marker='.',linestyle='--',linewidth=2,mark
 #here 'r.-' is in the format of [color][marker][line]
 
 
+##Second Line
+x2=np.arange(0,4.5,0.5)
+plt.plot(x2[:6],x2[:6]**2,'r',label='x^2')
+plt.plot(x2[5:],x2[5:]**2,'r--')
+
+
 plt.title("simple graph",fontdict={'fontname':'Comic Sans MS','fontsize':12,'fontweight':'bold','color':'red'})
 #fontdict contains the style for title of graph. fontname is the font style.
+
 plt.xlabel("X-axis",fontdict={'fontsize':10})#label for X-axis.
 plt.ylabel("Y-axis",fontdict={'fontsize':10})#label for Y-axis.
 
-plt.xticks([0,1,2,3,4])#set the positions of the ticks on the X-axis
-plt.yticks([0,2,4,6,8])#set the positions of the ticks on the Y-axis
+plt.xticks([0,0.5,1,1.5,2,2.5,3,3.5,4])#set the positions of the ticks on the X-axis
+plt.yticks([0,2,4,6,8,10,12,14,16])#set the positions of the ticks on the Y-axis
 #ticks are numbers or marks shown on the axis.
+
+
 plt.legend()
+
+plt.savefig('simple_graph.png',dpi=300)
+
 plt.show()
 #plt.show print/show the graph
+
+
+
+### Bar Charts
